bb25b5bd026c687cf29dabcc46ac69a68059aa22
Yasser <yasserkickboxing@gmail.com>
2020-01-31 20:35:05 +0100

    update body renaming dependencies part 1

--- Gui/opensim/view/src/org/opensim/view/nodes/OneBodyRenameAction.java
+++ Gui/opensim/view/src/org/opensim/view/nodes/OneBodyRenameAction.java
@@ -30,6 +30,7 @@ import org.openide.nodes.Node;
 import org.openide.util.HelpCtx;
 import org.openide.util.NbBundle;
 import org.openide.util.actions.CallableSystemAction;
+import org.opensim.modeling.Joint;
 import org.opensim.modeling.Model;
 import org.opensim.modeling.OpenSimObject;
 import org.opensim.view.ExplorerTopComponent;
@@ -40,6 +41,10 @@ import org.opensim.view.nodes.OneModelNode;
 import org.opensim.view.nodes.OpenSimObjectNode;
 import org.opensim.view.pub.OpenSimDB;
 import org.opensim.view.pub.ViewDB;
+import org.opensim.modeling.JointSet;
+import org.opensim.modeling.PhysicalFrame;
+import org.opensim.modeling.PhysicalOffsetFrame;
+import org.opensim.view.NavigatorByTypeModel;
 
 public final class OneBodyRenameAction extends CallableSystemAction {
    
@@ -52,6 +57,10 @@ public final class OneBodyRenameAction extends CallableSystemAction {
       Node[] selected = ExplorerTopComponent.findInstance().getExplorerManager().getSelectedNodes();
       if (selected.length == 1){
          OpenSimObjectNode objectNode = (OpenSimObjectNode) selected[0];
+         //selected body : the body to change the name of and update its dependencies
+         OneBodyNode selectedBody = (OneBodyNode) objectNode;
+         String oldNameOfTheOneBody = selectedBody.getName();
+         if(selectedBody == null) return;
          NotifyDescriptor.InputLine dlg =
                  new NotifyDescriptor.InputLine("Model Name: ", "Rename ");
          dlg.setInputText(objectNode.getOpenSimObject().getName());
@@ -69,13 +78,13 @@ public final class OneBodyRenameAction extends CallableSystemAction {
                  // The following is specific to renaming a model since
                  // other windows may display currentModel's name
                  // A more generic scheme using events should be used.
-                 if (objectNode instanceof OneModelNode) {
-                    Model dModel = ((OneModelNode)objectNode).getModel();
-                    if (dModel==OpenSimDB.getInstance().getCurrentModel())
-                       OpenSimDB.getInstance().setCurrentModel(dModel);   // Need to do this so that model dropdown updates
-                    // Mark the model as dirty
+                 if (objectNode instanceof OneBodyNode) {
+                     //getting the model to alter the model naming in the joints
+                    Model dModel = OpenSimDB.getInstance().getCurrentModel();
+                    // Mark the model as dirty to apply the changes to the sub elements
                     SingleModelGuiElements guiElem = OpenSimDB.getInstance().getModelGuiElements(dModel);
                     guiElem.setUnsavedChangesFlag(true);
+                    //updateOneBodyDependencies(selectedBody, dModel, oldNameOfTheOneBody,newName);
                  }
                  objectNode.refreshNode();
              } else
@@ -85,6 +94,43 @@ public final class OneBodyRenameAction extends CallableSystemAction {
       } else { // Should never happen
          DialogDisplayer.getDefault().notify(new NotifyDescriptor.Message("Rename of multiple objects is not supported."));
       }
+   }
+   //to update the naming of the body after the renaming
+   private void updateOneBodyDependencies(OneBodyNode bodyNode,Model parentModel,String oldBodyName,String newBodyName){
+       JointSet modelJoints  = parentModel.updJointSet();
+       
+       for (int i = 0; i < modelJoints.getSize(); i++) {
+           
+            Joint joint_i = modelJoints.get(i);
+            PhysicalFrame parentFrame =  joint_i.getParentFrame();
+            PhysicalFrame childFrame = joint_i.getChildFrame();
+            boolean noMoreFrames = false;
+            int j = 0;
+            while(!noMoreFrames)
+            {
+                try {
+                     PhysicalOffsetFrame offsetFrame = joint_i.get_frames(j);
+                     //System.out.println("Parent Frame Name");
+                     //System.out.println("*** "+offsetFrame.getParentFrame().getName()+" "+newBodyName);
+                     if(offsetFrame.getParentFrame().getName().equals(newBodyName)){
+                        System.out.println("this frame is altered by the renaming "+offsetFrame.getName()+" for the old name"+oldBodyName
+                        +"and this is the property "+offsetFrame.getPropertyByName("socket_parent"));
+                        //offsetFrame.updPropertyByName("socket_parent").setValueAsObject(bodyNode.getOpenSimObject());
+                        //offsetFrame.updateXMLNode(null);                                
+                        //offsetFrame.updPropertyByName("").
+                     }
+
+
+
+                } catch (Exception e) {
+                    noMoreFrames = true;
+                }
+                j++;
+                //fatal error after two iterarion fix
+                if(j==2) noMoreFrames = true;
+            }          
+       }
+       
    }
    
    @Override
